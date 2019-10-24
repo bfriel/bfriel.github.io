a02195371998e2938b863f3ca01ecea6b426ae9c
Brian Friel <brianfriel@Brians-Air.castle.associates>
2019-10-24 11:23:18 -0700

    Poll Google Books API for Reading List Data
    This swaps out the Goodreads API for Google Books for better book image coverage and quality.
    
    Initially I just fetched this info from the client, since no Gatsby plugin exists for Google Books. However, I decided this was a good time to try my hand at building GraphQL nodes.
    
    With this, book data comes at build time rather than run time! Images at still processed at run time though.
    
    To do:
    Clean up old Goodreads npm packages,
    Consider preprocessing book images to be in line with the rest of the site,
    Consider rewriting my sourceNodes export for an open source Google Books plugin

--- src/pages/reading-list.js
+++ src/pages/reading-list.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import { Link, graphql } from "gatsby";
-import BackgroundImage from "gatsby-background-image";
 import styles from './index-css-modules.module.css';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 export default function ReadingList({ data, location }) {
-    const currentlyReadingReviews = data.shelves.edges.filter(shelf => shelf.node.name === "currently-reading")[0].node.reviews;
-    const changedTheWayIThinkReviews = data.shelves.edges.filter(shelf => shelf.node.name === "changed-the-way-i-think")[0].node.reviews;
-    console.log('currentlyReadingReviews', currentlyReadingReviews)
-    console.log('changedTheWayIThinkReviews', changedTheWayIThinkReviews)
-    return (
-        <Layout location={location} title={data.site.siteMetadata.title}>
-            <SEO title="Reading List" />
-            <h1 className={styles.title}>Currently Reading</h1>
-        {currentlyReadingReviews.map(review => <img width={`200px`} key={review.id} src={review.book.image_url} />)}
+  const allBooksImReadingNow = data.allBooksImReadingNow.edges;
+  const allBooksThatChangedTheWayIThink = data.allBooksThatChangedTheWayIThink.edges;
 
-            {/* {currentlyReadingReviews.map(review =>  <BackgroundImage
-              Tag="div"
-              // fluid={data.avatar.childImageSharp.fluid}
-              style={{ height: `200px`, width: `100%`, maxWidth: `200px`, float: `left`, marginBottom: `10px`, marginRight: `10px`}}
-              backgroundColor={`#040e18`}
-            />)} */}
-            <h1 className={styles.title}>Changed the Way I Think</h1>
-            {changedTheWayIThinkReviews.map(review => <img key={review.id} src={review.book.image_url} />)}
-            {/* {changedTheWayIThinkReviews.map(review => <p>{review.book.title}</p>)} */}
-        </Layout>
-    )
+  return (
+      <Layout location={location} title={data.site.siteMetadata.title}>
+          <SEO title="Reading List" />
+          <h1 className={styles.title}>Currently Reading</h1>
+          <section style={{ display: `flex`, flexWrap: `wrap`, justifyContent: `space-evenly` }}>
+            {allBooksImReadingNow.map(book => <div key={book.node.id} style={{ height: `200px`, width: `130px`, margin: `10px`, backgroundImage: `url(${book.node.volumeInfo.thumbnail})`, backgroundSize: `cover`, }} />)}
+          </section>
+
+
+          <h1 className={styles.title}>Changed the Way I Think</h1>
+          <section style={{display: `flex`, flexWrap: `wrap`, justifyContent: `space-evenly`}}>
+          {allBooksThatChangedTheWayIThink.map(book => <div key={book.node.id} style={{ height: `200px`, width: `130px`, margin: `10px`, backgroundImage: `url(${book.node.volumeInfo.thumbnail})`, backgroundSize: `cover`, }} />)}
+          </section>
+      </Layout>
+  );
 };
 
 export const pageQuery = graphql`
@@ -52,112 +48,31 @@ export const pageQuery = graphql`
         }
       }
     }
-    shelves: allGoodreadsShelf {
+    allBooksImReadingNow {
       edges {
         node {
           id
-          name
-          exclusive
-          review_shelf_id
-          reviews {
-            id
-            rating
-            votes
-            spoiler_flag
-            spoilers_state
-            recommended_by
-            recommended_for
-            started_at
-            read_at
-            date_added
-            date_updated
-            read_count
-            body
-            comments_count
-            url
+          volumeInfo {
+            authors
             link
-            owned
-            book {
-              id, 
-              isbn, 
-              isbn13, 
-              text_reviews_count, 
-              uri, 
-              title, 
-              title_without_series, 
-              image_url, 
-              small_image_url, 
-              large_image_url, 
-              link, 
-              num_pages, 
-              format, 
-              edition_information, 
-              publisher, 
-              publication_day, 
-              publication_month, 
-              publication_year, 
-              average_rating, 
-              ratings_count, 
-              description,
-              published,            
-            }
+            thumbnail
+            title
+          }
+        }
+      }
+    }
+    allBooksThatChangedTheWayIThink {
+      edges {
+        node {
+          id
+          volumeInfo {
+            authors
+            link
+            thumbnail
+            title
           }
         }
       }
     }
   }
 `;
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-//       edges {
-//         node {
-//           excerpt
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             date(formatString: "MMMM DD, YYYY")
-//             title
-//             description
-//           }
-//         }
-//       }
-//     }
-//     books: allGoodreadsShelf {
-//       edges {
-//         node {
-//           id
-//           shelfName
-//           reviews {
-//             reviewID
-//             rating
-//             votes
-//             spoilerFlag
-//             spoilersState
-//             dateAdded
-//             dateUpdated
-//             book {
-//               bookID
-//               textReviewsCount
-//               isbn
-//               isbn13
-//               uri
-//               title
-//               titleWithoutSeries
-//               imageUrl
-//               smallImageUrl
-//               largeImageUrl
-//               description
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
