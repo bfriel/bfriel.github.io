15e8c812e40557a57839fb9326f38ab8be94d1a7
Brian Friel <brianfriel@Brians-MacBook-Air.local>
2019-10-29 19:38:56 -0700

    Refactor reading list author joining

--- src/pages/reading-list.js
+++ src/pages/reading-list.js
@@ -10,30 +10,46 @@ export default function ReadingList({ data, location }) {
   const allBooksImReadingNow = data.allBooksImReadingNow.edges;
   const allBooksThatChangedTheWayIThink = data.allBooksThatChangedTheWayIThink.edges;
 
-  const showBooks = books => books.map(book => 
-    <a 
-      key={book.node.id} 
-      className={styles.book}
-      href={book.node.volumeInfo.link}
-      target={`_blank`}
-      rel={`noopener noreferrer`}
-      style={{ 
-        display: `block`,
-        height: `200px`, 
-        width: `130px`, 
-        margin: `10px`, 
-        backgroundImage: `url(${book.node.volumeInfo.thumbnail})`, 
-        backgroundSize: `cover`, 
-        position: `relative`,
-        cursor: `pointer`,
-        color: `#000`,
-      }}>
-      <div className={styles.bookDetails}>
-        <p className={styles.bookTitle}>{book.node.volumeInfo.title}</p>
-        <p className={styles.bookAuthors}>{book.node.volumeInfo.authors.join(` & `)}</p>
-      </div>
-    </a>
-  );
+  const showBooks = (books) => books.map(book => {
+    const { authors, title, link, thumbnail } = book.node.volumeInfo;
+    return (
+      <a 
+        key={book.node.id} 
+        className={styles.book}
+        href={link}
+        target={`_blank`}
+        rel={`noopener noreferrer`}
+        style={{ 
+          display: `block`,
+          height: `200px`, 
+          width: `130px`, 
+          margin: `10px`, 
+          backgroundImage: `url(${thumbnail})`, 
+          backgroundSize: `cover`, 
+          position: `relative`,
+          cursor: `pointer`,
+          color: `#000`,
+        }}>
+        <div className={styles.bookDetails}>
+          <p className={styles.bookTitle}>{title}</p>
+          <p className={styles.bookAuthors}>{joinAuthors(authors)}</p>
+        </div>
+      </a>
+    );
+  });
+
+  const joinAuthors = (authors) => {
+    const numAuthors = authors.length - 1;
+    return authors.map((author, i) =>  {
+      if (i < numAuthors && i < numAuthors - 1) {
+        return `${author}, `;
+      } else if (i < numAuthors) {
+        return `${author} & `;
+      } else {
+        return author;
+      }
+    });
+  };
 
   return (
       <Layout location={location} title={data.site.siteMetadata.title}>
